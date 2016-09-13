f14744ca607e1f1341c404c1c83aa5fe08f790a0
Nate Denlinger <pitchinnate@gmail.com>
2016-09-13 11:41:24 -0400

    starting on model

--- framework/Eboo/Base/Model.php
+++ framework/Eboo/Base/Model.php
@@ -5,15 +5,29 @@ namespace Eboo\Base;
 class Model
 {
     protected $table;
+    private static $instance;
 
     public function __construct()
     {
-        echo "_connstruct called";
+        echo "_connstruct called <br>";
     }
 
     public static function findById($id)
     {
-        return ['id' => $id];
+        $model = self::getInstance();
+        return $model->query(['id'=>$id]);
     }
 
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function query($values)
+    {
+        return $values;
+    }
 }
---
+++ framework/Eboo/Factory/AppFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Eboo\Factory;
+
+use Eboo\App;
+
+class AppFactory
+{
+    private $app;
+
+    public static function getApp($config)
+    {
+        if (!self::$app)
+            self::$app = new App($config);
+        return self::$app;
+    }
+}
