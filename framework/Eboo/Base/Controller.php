f98f0c7353711228a1b630806341e7b7b1f62134
Nate Denlinger <pitchinnate@gmail.com>
2016-09-13 10:20:19 -0400

    wrong twig class used

--- framework/Eboo/Base/Controller.php
+++ framework/Eboo/Base/Controller.php
@@ -16,7 +16,7 @@ class Controller
     public function view($viewName, array $values = [])
     {
         $loader = new \Twig_Loader_Filesystem($this->app->getConfig('view.templatePath'));
-        $twig = new Twig_Environment($loader,[
+        $twig = new \Twig_Environment($loader,[
             'cache' => $this->app->getConfig('view.cachePath')
         ]);
         return $twig->render($viewName, $values);
